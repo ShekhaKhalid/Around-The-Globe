6969e693ce7640303a62efb931b612fe96c69d74
Nsreen Alfaifi <116817889+iNSREEN@users.noreply.github.com>
2024-05-02 00:40:10 +0300

    changes

---
+++ Assets/NsreenDesert/watchUI.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class watchUI : MonoBehaviour
+{
+    public GameObject canvas;
+
+    public void UIsetActeve()
+    {
+
+        canvas.SetActive(true);
+    }
+}
